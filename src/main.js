7fccec069ec5c602514783f399096ddf2cdb0e05
Skye <hkim7095@gmail.com>
2023-08-26 22:43:42 +0900

    movie search

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/index.js'
+// 특정한 폴더 안의 index파일을 가져올 때는 해당 이름 생략할 수 있음
+// import store from './store'
+import store from './store/index.js' 
 
 createApp(App)
   .use(router)
+  .use(store)
   .mount('#app')
