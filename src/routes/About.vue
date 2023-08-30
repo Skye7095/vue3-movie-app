786c818a242d81ef2e8cb70f553cad8e064da0c5
Skye <hkim7095@gmail.com>
2023-08-30 21:59:58 +0900

    about + 404

--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,7 +1,58 @@
 <template>
-  <h1>About!</h1>
+  <div class="about">
+    <div class="logo">
+      <img src="~assets/logo.png" :alt="name">
+    </div>
+    <div class="name">{{ name }}</div>
+    <div>{{ email }}</div>
+    <div>{{ blog }}</div>
+    <div>{{ phone }}</div>
+  </div>
 </template>
 
+<script>
+export default {
+  computed: {
+    name() {
+      return this.$store.state.about.name
+    },
+    email() {
+      return this.$store.state.about.email
+    },
+    blog() {
+      return this.$store.state.about.blog
+    },
+    phone() {
+      return this.$store.state.about.phone
+    }
+  }
+}
+</script>
+
 <style lang="scss">
-@import "~/scss/main";
+@import '~/scss/main';
+
+.about {
+  text-align: center;
+  .logo {
+    width: 250px;
+    height: 250px;
+    margin: 40px auto 20px;
+    padding: 30px;
+    border: 10px solid $gray-300;
+    border-radius: 50%;
+    box-sizing: border-box;
+    background-color: $gray-200;
+    position: relative;
+    img {
+      width: 100%;
+      padding: 10px;
+    }
+  }
+  .name {
+    font-size: 40px;
+    font-family: 'Oswald', sans-serif;
+    margin-bottom: 20px;
+  }
+}
 </style>
---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'Skye',
+    email: 'hkim7095@gmail.com',
+    blog: 'https://skye-coding.tistory.com/',
+    phone: '+82-1234-5678'
+  })
+}
